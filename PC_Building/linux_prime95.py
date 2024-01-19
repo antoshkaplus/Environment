38eaffa2f031939f1d2068a17412dd4d3db89192
Anton Logunov <antoshkaplus@gmail.com>
2024-01-19 07:53:05 -0800

    gogo

--- PC_Building/linux_prime95.py
+++ PC_Building/linux_prime95.py
@@ -2,7 +2,13 @@
 
 import subprocess
 
+# gnome-terminal does not understand `~`.
+# It opens user home by default though.
+prime_path = 'Tools/Prime95'
+
 subprocess.Popen("gnome-terminal --title=Vcore -- watch -n1 'echo \"scale=2; $(sudo rdmsr 0x198 -u --bitfield 47:32)/8192\" | bc'", shell=True)
 subprocess.Popen("gnome-terminal --title=Watt -- turbostat -s PkgWatt", shell=True)
 subprocess.Popen("gnome-terminal --title=Temp/Freq -- s-tui", shell=True)
-subprocess.Popen("./mprime -m", shell=True)
+subprocess.Popen(f"gnome-terminal --working-directory={prime_path} --title=Prime95 -- ./mprime -m", shell=True)
+tail = subprocess.Popen(f"tail -f ~/{prime_path}/results.txt", shell=True)
+tail.communicate()
